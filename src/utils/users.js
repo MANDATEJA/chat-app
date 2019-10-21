c3255cdb8880367cc4276d0358da16cfa55ea0bd
teja <tejareddy.teju@gmail.com>
2019-10-21 13:28:03 +0530

    initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,57 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if(!username || !room) {
+        return {
+            error: 'Username and Room are required'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if(existingUser) {
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0] // users.splice(index, 1) return an object with array(the array contains only one element as are removing only one element), we are accessing 0th element of the array
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) =>{
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
